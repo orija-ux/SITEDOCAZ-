5681d2a7da0d00e065faa4d35fb84fa95d430619
orija-ux <106121201+orija-ux@users.noreply.github.com>
2022-05-23 19:23:37 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const msg = document.querySelector(".options");
+
+ 
+function mudaParaSim() {
+	
+	document.getElementById("imgProfile").src = "imgs/channels4_profile.webp";
+	document.getElementById("msg").innerHTML = "Haha pegadinha do mallandro!";
+	
+	
+}
+
+
+function mudaParaNao() {
+	document.getElementById("imgProfile").src = "imgs/channels4_profile.webp";
+	document.getElementById("msg").innerHTML = "Ih mané o cara sabe!";
